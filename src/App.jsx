748cb7f2479bd44f61c64915f85151a97e209ffd
JavaBorka <borka34@gmail.com>
2023-10-24 09:42:15 +0200

    Import zadání

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import Candidate from './Candidate';
+import candidate01 from './assets/candidate01.png'
+import candidate02 from './assets/candidate02.png'
+import candidate03 from './assets/candidate03.png'
+import candidate04 from './assets/candidate04.png'
 
-function App() {
-  const [count, setCount] = useState(0)
+import './index.css';
 
+const App = () => {
+  const [candidates, setCandidates] = useState([]);
+  const [president, setPresident] = useState(null);
+
+  useEffect(() => setCandidates([
+    { name: "Ferdinand Mravenec", avatar: candidate01 },
+    { name: "Markéta Smetana", avatar: candidate02 },
+    { name: "Beáta Skočdopolová", avatar: candidate03 },
+    { name: "Lubomír Poňuchálek", avatar: candidate04 },
+  ]), []);
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="castle">
+        <div className="castle__image"></div>
+        <div className="castle__body">
+          <h1>Nový prezident</h1>
+          <p className="castle__president">
+            {
+              president === null ? 'Vyberte svého kandidáta' : president
+            }
+          </p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      
+      <h2>Kandidátí</h2>
+      <div className="candidate-list">
+        {candidates.map((c) => (
+          <Candidate 
+            key={c.name}
+            name={c.name} 
+            avatar={c.avatar} 
+          />
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
 export default App
